7135adc83460ee3bd262a5f919cb9fde909ebeda
Kyle McKenna <mckennakyler@gmail.com>
2020-01-26 20:44:16 +0000

    Automatically backed up by Learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -11,11 +11,3 @@ require 'pry'
 def get_the_keys(groceries)
   groceries.keys 
 end
-
-def get_the_min(groceries)
-  groceries.min  
-end
-
-def get_the_values(groceries)
-  groceries.values  
-end
